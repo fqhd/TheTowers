c218914f26018d35c1bf5272b9d2a7324de8689a
fqhd <iprograminjava@gmail.com>
2020-11-25 16:32:47 +0100

    Fixed ID

--- Client/Game.cpp
+++ Client/Game.cpp
@@ -74,8 +74,8 @@ void Game::update(sf::Window& window, Settings& settings, InputManager& manager,
 		uint8_t b;
 		glm::vec3 position;
 		uint8_t id;
-		std::cout << (unsigned int)id << std::endl;
 		packet >> x >> y  >> z >> b >> position.x >> position.y >> position.z >> id;
+		std::cout << (unsigned int)id << std::endl;
 
 		auto it = m_clients.find(id);
 		if(it != m_clients.end()){
