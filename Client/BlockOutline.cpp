db4c1485d5e8c5b0d8581a7bd4419fe934db90a4
Fahd <fqhd31256@gmail.com>
2021-06-08 21:23:39 +0100

    improved build system

--- Client/BlockOutline.cpp
+++ Client/BlockOutline.cpp
@@ -1,27 +1,27 @@
 #include "BlockOutline.hpp"
 
 void BlockOutline::init(){
-     m_shader.init();
-     m_renderer.init();
+	m_shader.init();
+	m_renderer.init();
 }
 
 void BlockOutline::render(Player& player, Camera& camera){
 
 	//Checking if the player is facing a block in order to draw an outline
-     if(!player.visibleBlocks.lookingAtBlock || player.visibleBlocks.isInsideBlock) return;
+	if(!player.visibleBlocks.lookingAtBlock || player.visibleBlocks.isInsideBlock) return;
 
 	//Getting the face of the block that the player is facing
-     Face blockFace = getFace(player.visibleBlocks);
+	Face blockFace = getFace(player.visibleBlocks);
 
 	//Binding the shader, loading a couple uniforms and rendering a face based on the position of the block
-     m_shader.bind();
-     m_shader.loadProjectionMatrix(camera.getProjectionMatrix());
-     m_shader.loadViewMatrix(camera.getViewMatrix());
+	m_shader.bind();
+	m_shader.loadProjectionMatrix(camera.getProjectionMatrix());
+	m_shader.loadViewMatrix(camera.getViewMatrix());
 	m_shader.loadBlockPosition(player.visibleBlocks.breakableBlock); //We send the position of the block to the vertex shader which will get added to the vertices and form a face
 
-     m_renderer.renderFace(blockFace); //This functions only renders a 1 of the 6 faces of a cube based on the parameter given
+	m_renderer.renderFace(blockFace); //This functions only renders a 1 of the 6 faces of a cube based on the parameter given
 
-     m_shader.unbind();
+	m_shader.unbind();
 
 }
 
