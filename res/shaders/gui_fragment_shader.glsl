86bf3cb82a12e7ff63c1e5909587d1aa6b9b2812
fqhd <fahd31256@gmail.com>
2021-07-09 15:15:38 +0100

    Added texture support into guirenderer

--- res/shaders/gui_fragment_shader.glsl
+++ res/shaders/gui_fragment_shader.glsl
@@ -1,9 +1,12 @@
 #version 330 core
 
 in vec4 pass_color;
+in vec3 pass_textureData;
 
-out vec4 out_Color;
+out vec4 out_color;
+
+uniform sampler2DArray textureMap;
 
 void main() {
-    out_Color = pass_color;
+    out_color = pass_color * texture(textureMap, pass_textureData);
 }
