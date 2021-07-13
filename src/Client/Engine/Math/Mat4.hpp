7432d0ecbf2f62f1421651952173b6942f666587
iluvpy <miomanns265@gmail.com>
2021-07-13 23:19:22 +0200

    made mat4 template for more data types

--- src/Client/Engine/Math/Mat4.hpp
+++ src/Client/Engine/Math/Mat4.hpp
@@ -1,13 +1,35 @@
 #pragma once
 
 namespace math {
-	  // matrices
-	struct mat4 {
-		mat4();
-		mat4(float n); // inits all values to param f
-		float m[4][4];
+	// matrices
+	template<typename T>
+	struct base_mat4 {
+		T m[4][4];
+		base_mat4() {
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					m[i][j] = 0.0f;
+				}
+			}
+		}
+		// inits all values to val
+		base_mat4(T val) {
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					m[i][j] = val;
+				}
+			}
+		}
 
-		void setIdentity();
+		void setIdentity(){
+			m[0][0] = (T)1.0f;
+			m[1][1] = (T)1.0f;
+			m[2][2] = (T)1.0f;
+			m[3][3] = (T)1.0f;
+		}
+
 
 	};
+
+	typedef base_mat4<float> mat4;
 }
