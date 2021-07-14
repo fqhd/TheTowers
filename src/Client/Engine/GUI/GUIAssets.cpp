86a613baae6897287e96a744e1874570fadc0dc9
fqhd <fahd31256@gmail.com>
2021-07-14 13:53:17 +0100

    Added gui assets class

---
+++ src/Client/Engine/GUI/GUIAssets.cpp
@@ -0,0 +1,47 @@
+#include "GUIAssets.hpp"
+
+void GUIAssets::init(){
+	m_hotbar = loadTexture("res/textures/gui/hotbar.png");
+	m_selector = loadTexture("res/textures/gui/hotbar_selector.png");
+	m_crosshair = loadTexture("res/textures/gui/crosshair.png");
+}
+
+GLuint GUIAssets::getHotbarTexture(){
+	return m_hotbar;
+}
+
+GLuint GUIAssets::getSelectorTexture(){
+	return m_selector;
+}
+
+GLuint GUIAssets::getCrosshair(){
+	return m_crosshair;
+}
+
+void GUIAssets::destroy(){
+	glDeleteTextures(1, &m_hotbar);
+	glDeleteTextures(1, &m_selector);
+	glDeleteTextures(1, &m_crosshair);
+}
+
+GLuint GUIAssets::loadTexture(const std::string& path){
+	Image image;
+	image.loadFromFile(path);
+
+	GLuint tID;
+	glGenTextures(1, &tID);
+	glBindTexture(GL_TEXTURE_2D, tID);
+
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.getWidth(), image.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, image.getData());
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	image.free();
+
+	return tID;
+}
