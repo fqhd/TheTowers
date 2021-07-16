54dcc275fbdd70c7750e194d2cfd5803251fcadf
iluvpy <miomanns265@gmail.com>
2021-07-16 14:42:12 +0200

    added GUILabel

--- src/Client/Engine/GUI/GUIInput.cpp
+++ src/Client/Engine/GUI/GUIInput.cpp
@@ -5,6 +5,8 @@ GUIInput::GUIInput(const math::vec4& destRect) {
 	m_destRect = destRect;
 	m_outlineRect = math::vec4(destRect.x-2, destRect.y-2, destRect.z+4, destRect.w+4);
 
+	m_input = "";
+	m_label.init(m_input, math::vec2(m_destRect.x, m_destRect.y), ColorRGBA8());
 	// fill vector with all keys that are needed i.e
 	// 0-25 (abc) 26-35 (number) 
 	// XXX add more keys
@@ -19,11 +21,15 @@ GUIInput::GUIInput(const math::vec4& destRect) {
 
 void GUIInput::update(InputManager* _manager) {
 	math::ivec2 mousePos = _manager->getMousePosition();
-	if (Utils::isInside(mousePos, m_destRect)) {
-		if (_manager->isButtonPressed(sf::Mouse::Left)) {
+	if (_manager->isButtonPressed(sf::Mouse::Left)) {
+		if (Utils::isInside(mousePos, m_destRect)) {
 			m_focused = m_focused ? false : true;
 		}
+		else {
+			m_focused = false;
+		}
 	}
+	
 	
 	// handle key presses when focues
 	if (m_focused) {
@@ -50,17 +56,17 @@ void GUIInput::update(InputManager* _manager) {
 				case sf::Keyboard::Space:
 					m_input += ' ';
 				case sf::Keyboard::Backspace:
-					m_input = m_input.substr(0, m_input.size()-1); // remove last char
+					m_input.pop_back(); // remove last char
 				default:
 					break;
 			}
 		}
-
-		printf("current input %s\n", m_input.c_str());
+		m_label.setString(m_input);
 	}
 }
 
 void GUIInput::render(GUIRenderer* renderer) {
 	renderer->draw(m_outlineRect, (m_focused ? ColorRGBA8() : ColorRGBA8(100, 100, 100, 255)), 0);
 	renderer->draw(m_destRect, ColorRGBA8(50, 50, 50, 255), 0);
+	m_label.render();
 }
