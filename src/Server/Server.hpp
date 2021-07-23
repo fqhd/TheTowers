03e6519fbdf852f7fc4693ff0359e09f095e0c23
iluvpy <miomanns265@gmail.com>
2021-07-23 15:43:50 +0200

    tidy up Server

---
+++ src/Server/Server.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <thread>
+#include <SFML/Network.hpp>
+#include "Config.hpp"
+#include "WorldData.hpp"
+#include "Structs.hpp"
+
+class Server {
+
+public:
+	void start();	
+
+private:
+	// private members
+	uint8_t* worldData = nullptr;
+	std::vector<Client> clients;
+	Config config;
+	bool isDone = false;
+	
+	//private functions
+	uint8_t createUniqueID();
+	bool doesIDExist(uint8_t id);
+	void addClient(sf::TcpListener& listener, sf::SocketSelector& selector);
+	void udpThread();
+	void tcpThread();
+	void compressAndSendWorld();
+	void freeWorldData();
+	uint8_t getReceivedPacket(sf::SocketSelector& selector, sf::Packet& packet, unsigned int& _senderIndex);
+	void sendPacketToOtherClients(sf::Packet& packet, uint8_t senderID);
+	void sendPacketToAllClients(sf::Packet& packet);
+	void disconnectPlayer(sf::SocketSelector& _selector, unsigned int _playerID);
+	void generateWorld();
+	void setBlock(int _x, int _y, int _z, uint8_t _block);
+	void updateWorldWithBlockUpdatePacket(sf::Packet& _packet);
+	void addCodeToBlockUpdatePacket(sf::Packet& _packet);
+};
