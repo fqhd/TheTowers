b7762b7b0024633dc50070b6e6528673c5f6e51b
iluvpy <miomanns265@gmail.com>
2021-07-26 19:30:59 +0200

    changed y speed while flying

--- src/Client/Player.cpp
+++ src/Client/Player.cpp
@@ -68,14 +68,14 @@ void Player::kbHandler(const Camera& camera, World* world, InputManager* _iManag
 	
 	if (_iManager->isKeyDown(sf::Keyboard::Space)) {
 		if (GameModeCanFly(m_gamemode))
-			m_velocity.y += 1*deltaTime;
+			m_velocity.y += 0.375f*deltaTime;
 		else if (m_canJump) 
 			m_velocity.y = 0.12f;
 	}
 
 	if (GameModeCanFly(m_gamemode)) {
 		if (_iManager->isKeyDown(sf::Keyboard::LControl)) {
-			m_velocity.y -= 1*deltaTime;
+			m_velocity.y -= 0.375f*deltaTime;
 		}
 	}
 
