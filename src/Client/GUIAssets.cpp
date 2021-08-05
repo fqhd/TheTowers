d646ade154426baf3d3e2b35e3f5bf04f53363d6
fqhd <fahd31256@gmail.com>
2021-08-05 20:13:45 +0100

    Improved Hotbar

--- src/Client/GUIAssets.cpp
+++ src/Client/GUIAssets.cpp
@@ -23,8 +23,8 @@ GLuint GUIAssets::loadTexture(const std::string& _path){
 	glGenTextures(1, &tID);
 	glBindTexture(GL_TEXTURE_2D, tID);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
--- src/Client/HUD.cpp
+++ src/Client/HUD.cpp
@@ -1,5 +1,8 @@
 #include "HUD.hpp"
 
+const float HOTBAR_WIDTH = 472 * 2;
+const float HOTBAR_HEIGHT = 52 * 2;
+
 void HUD::render(GUIRenderer* _renderer, Config* _config){
 	unsigned int ww = _config->getWindowWidth();
 	unsigned int wh = _config->getWindowHeight();
@@ -12,6 +15,5 @@ void HUD::renderHotbar(GUIRenderer* _renderer, Config* _config){
 	unsigned int ww = _config->getWindowWidth();
 	unsigned int wh = _config->getWindowHeight();
 
-	_renderer->drawRect(Utils::mapDestRect(math::vec4(724, 4, 472, 52), 1920, 1080, ww, wh), math::vec4(0, 0, 1, 1), _renderer->assets.getHotbarTexture(), ColorRGBA8()); // Rendering the crosshair
-	// _renderer->drawRect(math::vec4(488, 4, 944, 104), math::vec4(0, 0, 1, 1), _renderer->assets.getHotbarTexture(), ColorRGBA8()); // Rendering the crosshair
+	_renderer->drawRect(Utils::mapDestRect(math::vec4(1920 / 2 - HOTBAR_WIDTH / 2, 4, HOTBAR_WIDTH, HOTBAR_HEIGHT), 1920, 1080, ww, wh), math::vec4(0, 0, 1, 1), _renderer->assets.getHotbarTexture(), ColorRGBA8()); // Rendering the hotbar
 }
