b2615a549a0d7c60610b6655c17bef49a0e303f7
fqhd <fahd31256@gmail.com>
2022-05-06 10:14:54 +0100

    fix: add cFlags to create_build_files.py

--- create_build_files.py
+++ create_build_files.py
@@ -1,6 +1,7 @@
 import os
 
-def getLFlags():
+# Functions
+def getLFlags(userOS):
     lFlags = 'lflags = '
     if(userOS == 'macos'):
         files = os.listdir('./deps/macos')
@@ -9,16 +10,42 @@ def getLFlags():
             lFlags += ' -l' + f[3:-6]
         lFlags += ' -framework OpenGL'
     elif(userOS == 'linux'):
+        # Not supported currently
         pass
     elif(userOS == 'windows'):
+        # Not supported currently
         pass
     return lFlags
 
-userOS = input("What is your operating system?(linux/windows/macos): ")
+def getUserOS():
+    userOS = input("What is your operating system?(linux/windows/macos): ")
+    if(userOS != 'windows' and userOS != 'macos' and usrOS != 'linux'):
+        print(userOS)
+        print('Incorrect OS entered')
+        exit()
+    return userOS
 
-if(userOS != 'windows' and userOS != 'macos' and usrOS != 'linux'):
-    print(userOS)
-    print('Incorrect OS entered')
-    exit()
+def getSourceDirs():
+    sourceDirs = []
+    sourceDirs.append('./src/Client/Engine')
+    sourceDirs.append('./src/Client/Game')
+    sourceDirs.append('./src/Client/glad')
+    sourceDirs.append('./src/Client/GUI')
+    sourceDirs.append('./src/Client/Input')
+    sourceDirs.append('./src/Client/Math')
+    return sourceDirs
 
-print(getLFlags())
+def getCFlags(sourceDirs):
+    cFlags = 'cflags = -std=c++17 -O0 -fdiagnostics-color=always -g -m64 -I./deps/include'
+    for dir in sourceDirs:
+        cFlags += ' -I' + dir
+    return cFlags
+
+def main():
+    userOS = getUserOS()
+    sourceDirs = getSourceDirs()
+    cFlags = getCFlags(sourceDirs)
+    lFlags = getLFlags(userOS)
+    print(cFlags)
+
+main()
