1f13ed1175ac3b3a03b0136e2c266f0a47e26536
Fahd <fahd31256@gmail.com>
2022-12-03 04:31:26 +0000

    FilePathManager class

---
+++ src/Client/Engine/FilePathManager.cpp
@@ -0,0 +1,23 @@
+#include "FilePathManager.hpp"
+#include <filesystem>
+
+std::string rootDirectory;
+
+std::string getProjectRootDirectory(){
+	std::string path;
+	std::cout << "Enter your root folder: ";
+	std::cin >> path;
+	while(!std::filesystem::is_directory(std::filesystem::path(path))){
+		std::cout << "That is not a valid path, try again: ";
+		std::cin >> path;		
+	}
+	return path;
+}
+
+void FilePathManager::init(){
+    rootDirectory = getProjectRootDirectory();
+}
+
+const std::string& FilePathManager::getRootFolderDirectory(){
+    return rootDirectory;
+}
---
+++ src/Client/Engine/FilePathManager.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+
+namespace FilePathManager {
+    void init();
+    const std::string& getRootFolderDirectory();
+};
