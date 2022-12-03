f354a3b0656204f96fd4797f7b75949077e8c2b3
Fahd <fahd31256@gmail.com>
2022-12-03 05:03:33 +0000

    Better FilePathManager message

--- src/Client/Engine/FilePathManager.cpp
+++ src/Client/Engine/FilePathManager.cpp
@@ -5,7 +5,9 @@ std::string rootDirectory;
 
 std::string getProjectRootDirectory(){
 	std::string path;
-	std::cout << "Enter your root folder: ";
+	std::cout << "This is a workaround to executables ending up in different folder locations on different Operating Systems." << std::endl;
+	std::cout << "You will only be prompted to enter this once." << std::endl;
+	std::cout << "Enter the absolute path to the root folder of the project: ";
 	std::cin >> path;
 	while(!std::filesystem::is_directory(std::filesystem::path(path))){
 		std::cout << "That is not a valid path, try again: ";
